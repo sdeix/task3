4994128b41f4bc37b420b75586a4e111de52161f
sdeix <sdeix2765@gmail.com>
2023-02-09 11:41:57 +0700

    функционал создания карточек

--- js/main.js
+++ js/main.js
@@ -4,52 +4,52 @@ let eventBus = new Vue()
 Vue.component('board',{
 template:`
 <div id=board>
-<div class="column1">
+<div class="column" class="column1">
 
-
-</div>
-<card></card>
       <form>
-      <!--      <div id="upperFormDiv">
+           <div id="upperFormDiv">
           <div>
               <p>Название карточки:</p>
-              <input type="text" placeholder="Название" v-model="cardTitle" maxlength="15">
+              <input type="text" placeholder="Название" v-model="title" maxlength="15">
           </div>
-          <p>Количество заметок в карточке:</p>
-          <input type="number" max="5" min="3" v-model="numberOfOptions">
+          <div>
+          <p>Описание задачи:</p>
+          <input type="text" placeholder="Описание" v-model="desc" maxlength="60">
+          </div>
+          <div>
+          <input type="date" id="start" name="trip-start" v-model="deadlin"
+
+          min="2023-01-01" max="2030-12-31">
+          </div>
           
-          
-          <input type="submit" value="Создать пункты" @click.prevent="makeOptionsArr" maxlength="20">
       </div>
-      <input v-for="i in options" :key="i" class="pointInput" type="text" placeholder="пункт">
-      <input type="submit" @click.prevent="createCard" value="Создать карточку"> -->
+      <p>Время дедлайна</p>
+      <input type="submit" @click.prevent="createCard" value="Создать карточку"> 
       </form>
-<div class="column2">
+      </div>
+      <card v-for="(card, index) in column1" :createdate="card.createdate" :title="card.title" 
+      :desc="card.desc" :deadlineYear="card.deadline[index].year" :deadlineMonth="card.deadline[index].month" :deadlineDay="card.deadline[index].day" :lastediting="card.lastediting">  </card>
+<div class="column" class="column2">
 
 
 </div>
 
 
-<div class="column3">
+<div class="column" class="column3">
 
 
 </div>
 
 
-<div class="column4">
+<div class="column" class="column4">
 
 
 </div>
 
-<!--
-<input type="date" id="start" name="trip-start" v-model="date2"
 
-       min="2018-01-01" max="2018-12-31">
-       <button
-       v-on:click="But()"
- >кнопка
-</button>
--->
+
+
+
 </div>
 
 
@@ -63,6 +63,16 @@ data(){
             column2:[],
             column3:[],
             column4:[],
+            points:[],
+            year:"",
+            month:"",
+            day:"",
+
+            title:"",
+            desc: "",
+            deadline: [],
+            deadlin:2,
+            lastediting: null,
       }
   },
 methods:{
@@ -75,6 +85,21 @@ methods:{
             console.log(this.date1)            
             // console.log(this.q)
             // console.log(this.date2.getFullYear())
+      },
+      createCard(){
+            if(typeof(this.deadlin) != "number"){
+                  this.year = this.deadlin[0] + this.deadlin[1] + this.deadlin[2] + this.deadlin[3]
+                  this.month = this.deadlin[5] + this.deadlin[6]
+                  this.day= this.deadlin[8] + this.deadlin[9]
+                  
+                  this.points.splice(0,this.points.length)
+                  this.deadline.push({day:this.day, month:this.month, year:this.year})
+                  var now = new Date()
+                  now = String(now)
+                  this.column1.push({createdate:now, title:this.title ? this.title: "Без названия", desc:this.desc ? this.desc:"Без описания", lastediting:now, deadline:this.deadline })
+                  console.log(this.column1)
+            }
+            
       }
 },
 
@@ -83,12 +108,12 @@ methods:{
 
 Vue.component('card',{
       template:`
-      <div id=card>
-      <p class="createdate">{{createdate}}</p>
-      <p class="cardTitle">{{title}}</p>
-      <p class="desc">{{desc}}</p>
-      <p class="deadline">{{deadline}}</p>
-      <p class="last">{{lastediting}}</p>
+      <div class=card>
+      <p class="createdate">Дата создания: {{createdate}}</p>
+      <p class="cardTitle">Название: {{title}}</p>
+      <p class="desc">Описание: {{desc}}</p>
+      <p class="deadline">Дэдлайн: {{deadlineDay}}-{{deadlineMonth}}-{{deadlineYear}}</p>
+      <p class="last" v-if="lastediting" >Время последнего редактирования: {{lastediting}}</p>
       
              
       
@@ -97,17 +122,27 @@ Vue.component('card',{
       `,
       data(){
             return{
-                  createdate:new Date(),
-                  title:"название",
-                  desc:"description",
-                  deadline:"2012-12-12",
-                  lastediting:"last",
+
 
             }
         },
       methods:{
-      }    
-      })
+      },
+      props:{
+            createdate:Date(),
+            title: String,
+            desc:String,
+            deadlineYear:String,
+            deadlineMonth:String,
+            deadlineDay:String,
+
+            lastediting:String,
+
+      }
+
+
+
+})
 
 
 
