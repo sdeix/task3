a66ba17a0e974faa3a149c14f6ca7147db1b92d2
sdeix <sdeix2765@gmail.com>
2023-02-10 11:18:30 +0700

    перемещение карточек по стобцам и создание комментария в третьем

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ Vue.component('board',{
 template:`
 <div id=board>
 <div  class="column column1">
-
+<h2>Запланированные задачи</h2>
       <form>
            <div id="upperFormDiv">
           <div>
@@ -30,18 +30,21 @@ template:`
       <card v-for="card in column1" :pointss="card"> </card>
       </div>
       <div  class="column column2">
+      <h2>Задачи в работе</h2>
       <card v-for="card in column2" :pointss="card"> </card>
 
 </div>
 
 
 <div  class="column column3">
+<h2>Тестирование</h2>
 <card v-for="card in column3" :pointss="card"> </card>
 
 </div>
 
 
 <div  class="column column4">
+<h2>Выполненные задачи</h2>
 <card v-for="card in column4" :pointss="card"> </card>
 
 </div>
@@ -68,6 +71,7 @@ data(){
             month:"",
             day:"",
 
+            move : "",
             title:"",
             desc: "",
             deadline: [],
@@ -86,7 +90,8 @@ methods:{
                   this.deadline.push({day:this.day, month:this.month, year:this.year})
                   var now = new Date()
                   now = String(now)
-                  this.column1.push({createdate:now, title:this.title ? this.title: "Без названия", desc:this.desc ? this.desc:"Без описания", lastediting:now, deadline:this.deadline, delete:false, column:1, edit:false})
+                  this.column1.push({createdate:now, move:this.move, title:this.title ? this.title: "Без названия", desc:this.desc ? this.desc:"Без описания", 
+                  lastediting:now, deadline:this.deadline, delete:false, column:1, edit:false, comment:""})
                   this.deadline=[]
             }
             
@@ -99,7 +104,43 @@ mounted(){
                         this.column1.splice(i, 1)
 
             }}
+      }),
+      eventBus.$on('OneToTwo', movecard1 => {
+            for(let i = 0; i < this.column1.length; i++){
+                  if(this.column1[i].move == "two"){
+                        this.column1[i].move = ""
+                        this.column1[i].column = 2
+                        this.column2.push(this.column1[i])
+                        this.column1.splice(i, 1)
+                        console.log(this.column2)
+                        
+
+            }}
+      }),
+      eventBus.$on('TwoToThree', movecard2 => {
+            for(let i = 0; i < this.column2.length; i++){
+                  if(this.column2[i].move == "three"){
+                        this.column2[i].move = ""
+                        this.column2[i].column = 3
+                        this.column3.push(this.column2[i])
+                        this.column2.splice(i, 1)
+                        
+
+            }}
+      }),
+      eventBus.$on('ThreeToTwo', movecard3 => {
+            for(let i = 0; i < this.column3.length; i++){
+                  if(this.column3[i].move == "two"){
+                        this.column3[i].move = ""
+                        this.column3[i].column = 2
+                        this.column2.push(this.column3[i])
+                        this.column3.splice(i, 1)
+
+                        
+
+            }}
       })
+
 },
 watch:{
       column1(){
@@ -127,15 +168,33 @@ Vue.component('card',{
       <button    @click="pointss.edit=false"> Отмена </button>
       <button    @click="pointss.edit=false" v-on:click="Edit()"> Подтвердить </button> </div>
       <div v-else >  </div>
-             
+
+      <div v-if="pointss.column==1">
+      <button    @click="OneToTwo()"> --></button>
+      </div>
+
+
+      <div v-if="pointss.column==2">
+      <button    @click="TwoToThree()"> --></button>
+      </div>
+
+      <div v-if="pointss.column==3">
+      <button    @click="ThreeToTwo()"> <-- </button>
+      <button    @click="ThreeToFour()"> --> </button>
       
-      
+      <p>Причина: <input type="text" v-model="newComment"></p>    
+      </div>
+
+
+      <div v-if="pointss.comment">
+      <p>Причина: {{pointss.comment}}</p>
       </div>
       `,
       data(){
             return{
                   newTitle:"",
                   newDesc: "",
+                  newComment:'',
             }
         },
       methods:{
@@ -144,8 +203,27 @@ Vue.component('card',{
                   eventBus.$emit('deleteCard')
             },
             Edit(){
+                  var now = new Date(),
+                  now = String(now)
+                  this.pointss.lastediting = now,
                   this.pointss.title = this.newTitle,
                   this.pointss.desc = this.newDesc
+            },
+            OneToTwo(){
+                  this.pointss.move = "two",
+                  eventBus.$emit('OneToTwo')
+            },
+            TwoToThree(){
+                  this.pointss.move = "three",
+                  eventBus.$emit('TwoToThree')
+
+            },
+            ThreeToTwo(){
+                  this.pointss.comment = this.newComment
+                  this.newComment = "",
+                  this.pointss.move = "two",
+                  eventBus.$emit('ThreeToTwo')
+
             }
       },    
       props:{
